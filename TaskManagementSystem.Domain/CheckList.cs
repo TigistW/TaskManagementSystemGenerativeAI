523674e0604cfb2aa878fd79d214e1e56824537c
Tigist Wondimneh <wondimnehtigist@gmail.com>
2023-05-18 15:06:02 +0300

    feat(backend-tigist): modify the checklist model

--- TaskManagementSystem.Domain/CheckList.cs
+++ TaskManagementSystem.Domain/CheckList.cs
@@ -4,6 +4,7 @@ namespace TaskManagementSystem.Domain;
 
 public class CheckList
 {
+    public int id {get; set;}
     public string Title { get; set; }
     public string Description { get; set; } = " ";
     public int ChoreId {get; set;}
